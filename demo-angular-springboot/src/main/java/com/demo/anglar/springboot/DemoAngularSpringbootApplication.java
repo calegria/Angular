8f20516701fc794c89d51843c61baa4accf79b5b
Carlos Alegria <calegria@asesoftware.com>
2021-04-06 08:34:20 -0500

    <

---
+++ demo-angular-springboot/src/main/java/com/demo/anglar/springboot/DemoAngularSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.demo.anglar.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAngularSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAngularSpringbootApplication.class, args);
+	}
+
+}
---
+++ demo-angular-springboot/src/test/java/com/demo/anglar/springboot/DemoAngularSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.anglar.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAngularSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
